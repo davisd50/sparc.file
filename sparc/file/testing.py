343229347dfc014a964cfa1ae6114a27b2e4f018
David Davis <davisd50@gmail.com>
2017-08-09 10:26:12 -0400

    fix name space var name

--- sparc/file/testing.py
+++ sparc/file/testing.py
@@ -1,4 +1,4 @@
 import sparc.file
 from sparc.testing.testlayer import SparcZCMLFileLayer
 
-SPARC_TAIL_INTEGRATION_LAYER = SparcZCMLFileLayer(sparc.file)
+SPARC_FILE_INTEGRATION_LAYER = SparcZCMLFileLayer(sparc.file)
